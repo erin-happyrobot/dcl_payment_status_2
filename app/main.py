a83f078640a212c87738e43088b2f0e449b017e3
Erin DeLong <erindelong@Mac.attlocal.net>
2025-09-03 08:03:36 -0700

    pulling runs and printing- no email

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from contextlib import asynccontextmanager
-from datetime import datetime
 import logging
+import requests 
+from requests.adapters import HTTPAdapter
+from datetime import timedelta, datetime, timezone
+import os
+import pandas as pd
+import dotenv
+
+
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
@@ -9,13 +16,57 @@ from fastapi import FastAPI
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("scheduler")
 
+dotenv.load_dotenv()
+
 
 scheduler = AsyncIOScheduler(timezone="UTC")
 
 
 def perform_scheduled_action() -> None:
-    """Replace this with your real job logic."""
-    logger.info("Running scheduled action at %s", datetime.utcnow().isoformat())
+    print(f"[scheduler] Running 12-hour task at {datetime.now(timezone.utc).isoformat()}")
+    session = requests.Session()
+    session.mount('https://', HTTPAdapter(max_retries=3))
+    url = "https://platform.happyrobot.ai/api/v1/runs"
+    headers = {
+        "Authorization": f"Bearer {os.getenv('PLATFORM_API_KEY')}",
+        "x-organization-id": os.getenv('DCL_ORG_ID'),
+    }
+    start_utc = datetime.now(timezone.utc) - timedelta(hours=12)
+    params = {
+        "limit": 100,
+        "use_case_id": os.getenv('PAYMENT_STATUS_USE_CASE_ID'),
+         "start": start_utc.isoformat(timespec="seconds").replace("+00:00", "Z")
+    }
+
+    response = session.get(url, headers=headers, params=params, timeout=(3.05, 20))
+    response.raise_for_status()
+    runs = response.json()
+    logger.info(f"[scheduler] Found {len(runs)} runs")
+    for run in runs:
+        print(f"[scheduler] Run {run['id']} - {run['status']}")
+
+
+    print(type(response.json()))
+    print(response.json()[0])
+    
+    calls_data = response.json()
+    df_total = pd.DataFrame(calls_data)
+    total_calls_past_12_hours = len(df_total)
+    df = pd.json_normalize(calls_data)
+    print(df.columns)
+    total_calls_past_12_hours_failed = len(df[df['data.01988326-54c7-7f8c-a055-6b5ea45d6cc8.response.classification'] == 'did_not_find_load'])
+    total_calls_past_12_hours_failed_percentage = total_calls_past_12_hours_failed / total_calls_past_12_hours
+    logger.info(f"[scheduler] Total calls past 12 hours: {total_calls_past_12_hours}")
+    logger.info(f"[scheduler] Total calls past 12 hours failed: {total_calls_past_12_hours_failed}")
+    logger.info(f"[scheduler] Total calls past 12 hours failed percentage: {total_calls_past_12_hours_failed_percentage}")
+
+    if total_calls_past_12_hours_failed_percentage > 0.25:
+        logger.info("[scheduler] Total calls past 12 hours failed percentage is greater than 25%")
+        logger.info(f"[scheduler] Sending email to {os.getenv('EMAIL_TO')}")
+        # send_email( [os.getenv('EMAIL_TO')], "Payment Status Audit Happy Robot", f"Total calls past 12 hours failed percentage is greater than 25%. We are seeing a rate of {total_calls_past_12_hours_failed_percentage*100}% of calls failing to find a load id.")
+    else:
+        logger.info("[scheduler] Total calls past 12 hours failed percentage is less than 25%")
+
 
 
 @asynccontextmanager
@@ -26,7 +77,7 @@ async def lifespan(app: FastAPI):  # type: ignore[override]
     scheduler.add_job(
         perform_scheduled_action,
         trigger="interval",
-        hours=12,
+        hours=0.002,
         id="twelve_hour_job",
         replace_existing=True,
         max_instances=1,
